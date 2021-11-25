c8d441b8f2c4988f8884fcb9528cca25b5005330
denizafontineles <denizafontineles@outlook.com>
2021-11-24 21:31:56 -0300

    adiciona LIB JAR

---
+++ src/com/deniza/utils/fila/integer/No.java
@@ -0,0 +1,13 @@
+package com.deniza.utils.fila.integer;
+
+public class No {
+
+	int dado;
+	No proximo;
+	
+	@Override
+	public String toString() {
+		return "Dado: "+dado;
+	}
+	
+}
---
+++ src/com/deniza/utils/fila/string/No.java
@@ -0,0 +1,13 @@
+package com.deniza.utils.fila.string;
+
+public class No {
+
+	String dado;
+	No proximo;
+	
+	@Override
+	public String toString() {
+		return "Dado: " + dado;
+	}
+	
+}
